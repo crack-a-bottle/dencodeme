1f4dcbef80d541c6f5863edf075a6fd57c3c2270
Justin Pridgen <justincpridgen672@gmail.com>
2023-04-08 15:50:54 -0400

    mmm

--- src/base.ts
+++ src/base.ts
@@ -5,17 +5,16 @@ export function base(radix: number): NumberSystem {
     const rdx = Math.floor(Math.min(36, Math.max(2, radix)));
     const max = (255).toString(rdx).length;
     const charset = "0123456789abcdefghijklmnopqrstuvwxyz".slice(0, rdx);
-    const regex = new RegExp(`[${charset}]`, "gi");
 
     return {
         charset,
         radix: rdx,
-        regex,
+        regex: new RegExp(`[${charset}]`, "gi"),
         encode(data, encoding = "utf8") {
             return Buffer.from(data.toString(encoding), encoding).toJSON().data.map(x => x.toString(rdx).padStart(max, "0")).join("");
         },
         decode(data, encoding) {
-            const charMatch = data.toString().toLowerCase().match(regex);
+            const charMatch = data.toString().toLowerCase().match(this.regex);
             if (charMatch == null) return "";
 
             const decoded = Buffer.from(util.padStart(charMatch, max).map(x => parseInt(x, rdx)));
